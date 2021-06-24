9f69d96e1278e5aaf61451fff2f0d0e378001c4e
Shalini Nangia <shalininangia@Shalinis-MacBook-Pro.local>
2021-06-24 22:47:21 +0100

    add variables on JS and round buttons CSS

--- app.dev.js
+++ app.dev.js
@@ -1,7 +1,16 @@
 "use strict";
 
 var buttons = document.querySelectorAll('button');
-var display = document.querySelector('.display1');
+var display = document.querySelector('.display');
+var operationBtns = document.querySelector('.operation__btn');
+var numBtns = document.querySelector('.num__btn');
+var divideBtn = document.querySelector('.divide__btn');
+var multiplyBtn = document.querySelector('.multiply__btn');
+var minusBtn = document.querySelector('.minus__btn');
+var plusBtn = document.querySelector('.plus__btn');
+var clearBtn = document.querySelector('.clear__btn');
+var equalsBtn = document.querySelector('.equals__btn');
+var decimalBtn = document.querySelector('.decimal__btn');
 
 var displayOnScreen = function displayOnScreen(e) {
   display.value = e.target.innerHTML;
--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const buttons = document.querySelectorAll('button');
-const display = document.querySelector('.display1');
+const display = document.querySelector('.display');
+const operationBtns = document.querySelector('.operation__btn');
+const numBtns = document.querySelector('.num__btn'); 
+const divideBtn = document.querySelector('.divide__btn')
+const multiplyBtn = document.querySelector('.multiply__btn');
+const minusBtn = document.querySelector('.minus__btn');
+const plusBtn = document.querySelector('.plus__btn');
+const clearBtn = document.querySelector('.clear__btn');
+const equalsBtn = document.querySelector('.equals__btn');
+const decimalBtn = document.querySelector('.decimal__btn')
+
 
 const displayOnScreen = (e) => {
   display.value = e.target.innerHTML
@@ -9,7 +19,6 @@ const displayOnScreen = (e) => {
 buttons.forEach((button) => {
   button.addEventListener ('click', displayOnScreen)
 })
-
 
 
 
