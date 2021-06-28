ca6e9bfc7d13ec6176bc32130224812c52c9b3a4
Shalini Nangia <shalininangia@Shalinis-MacBook-Pro.local>
2021-06-28 14:09:16 +0100

    the plus operation works on the console log

--- app.dev.js
+++ app.dev.js
@@ -1,9 +1,11 @@
 "use strict";
 
+function _readOnlyError(name) { throw new Error("\"" + name + "\" is read-only"); }
+
 var buttons = document.querySelectorAll('button');
 var display = document.querySelector('.display');
-var operationBtns = document.querySelector('.operation__btn');
-var numBtns = document.querySelector('.num__btn');
+var operationBtns = document.querySelectorAll('.operation__btn');
+var numBtns = document.querySelectorAll('.num__btn');
 var divideBtn = document.querySelector('.divide__btn');
 var multiplyBtn = document.querySelector('.multiply__btn');
 var minusBtn = document.querySelector('.minus__btn');
@@ -11,12 +13,45 @@ var plusBtn = document.querySelector('.plus__btn');
 var clearBtn = document.querySelector('.clear__btn');
 var equalsBtn = document.querySelector('.equals__btn');
 var decimalBtn = document.querySelector('.decimal__btn');
+var value = 0;
 
 var displayOnScreen = function displayOnScreen(e) {
-  display.value = e.target.innerHTML;
-  console.log(e.target.innerHTML);
+  if (value === 0) {
+    value = e.target.innerHTML;
+    display.value = value;
+    return value;
+  } else {
+    value = value + e.target.innerHTML;
+    display.value = value;
+  }
 };
 
 buttons.forEach(function (button) {
   button.addEventListener('click', displayOnScreen);
 });
+clearBtn.addEventListener('click', function () {
+  display.value = 0;
+});
+equalsBtn.addEventListener('click', function () {
+  var splitDisplay = display.value.split('');
+  var num1 = splitDisplay[0];
+  var operation = splitDisplay[1];
+  var num2 = splitDisplay[2];
+
+  _sum(num1, operation, num2);
+});
+
+var _sum = function sum(num1, operation, num2) {
+  if (operation === '+') {
+    console.log(Number(num1) + Number(num2));
+    return num1 + num2;
+  } else if (operation === '-') {
+    _sum = (_readOnlyError("sum"), num1 - num2);
+  } else if (operation === '*') {
+    _sum = (_readOnlyError("sum"), num1 * num2);
+  } else if (operation === "/") {
+    _sum = (_readOnlyError("sum"), num1 / num2);
+  }
+
+  return _sum;
+};
--- app.js
+++ app.js
@@ -1,24 +1,59 @@
 const buttons = document.querySelectorAll('button');
 const display = document.querySelector('.display');
-const operationBtns = document.querySelector('.operation__btn');
-const numBtns = document.querySelector('.num__btn'); 
+const operationBtns = document.querySelectorAll('.operation__btn');
+const numBtns = document.querySelectorAll('.num__btn'); 
 const divideBtn = document.querySelector('.divide__btn')
 const multiplyBtn = document.querySelector('.multiply__btn');
 const minusBtn = document.querySelector('.minus__btn');
 const plusBtn = document.querySelector('.plus__btn');
 const clearBtn = document.querySelector('.clear__btn');
 const equalsBtn = document.querySelector('.equals__btn');
-const decimalBtn = document.querySelector('.decimal__btn')
+const decimalBtn = document.querySelector('.decimal__btn');
 
+let value = 0;
 
 const displayOnScreen = (e) => {
-  display.value = e.target.innerHTML
-  console.log(e.target.innerHTML);
+  if(value === 0) {
+    value = e.target.innerHTML
+    display.value = value;
+    return value;
+  } else {
+    value = value + e.target.innerHTML
+    display.value = value;
+  }
 }
 
 buttons.forEach((button) => {
   button.addEventListener ('click', displayOnScreen)
 })
 
+clearBtn.addEventListener('click', () => {
+  display.value = 0;
+});
 
+equalsBtn.addEventListener('click', () => {
+ const splitDisplay = display.value.split('');
+ const num1 = splitDisplay[0];
+ const operation = splitDisplay[1];
+ const num2 = splitDisplay[2];
+ sum(num1, operation, num2)
+});
+
+
+  const sum = (num1, operation, num2) => {
+
+  if (operation === '+'){
+    console.log(Number(num1) + Number(num2));
+    return num1 + num2;
+  }else if (operation === '-'){
+    sum = num1 - num2;
+  }else if (operation === '*'){
+    sum = num1 * num2;
+  }else if (operation === "/"){
+    sum = num1 / num2;
+   }
+  return sum; 
+}
+
+ 
 
